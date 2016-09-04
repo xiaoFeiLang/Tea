6ecd6e953841150d01dac6df5276c46c9aadb57e
loulou <627265625@qq.com>
2016-09-04 22:21:31 +0800

    .

---
+++ Tea/Tea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tea
+//
+//  Created by loulou on 16/9/4.
+//  Copyright © 2016年 qxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tea/Tea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tea
+//
+//  Created by loulou on 16/9/4.
+//  Copyright © 2016年 qxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
